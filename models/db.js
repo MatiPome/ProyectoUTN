7436d1a0996637c420f5de8b382fe464f85df2f8
MatiPome <matiaspome95@gmail.com>
2022-08-22 01:34:45 -0300

    fixed db "mySQL" to "MYSQL"

--- models/db.js
+++ models/db.js
@@ -3,10 +3,11 @@ var util = require('util');
 
 var pool = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.mySQL_HOST,
-    user: process.env.mySQL_USER,
-    password: process.env.mySQL_PASSWORD,
-    database: process.env.mySQL_DB_NAME
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB_NAME,
+    port: 3306
 });
 
 pool.query = util.promisify(pool.query);
